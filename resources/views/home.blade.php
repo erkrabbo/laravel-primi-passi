b89ec28c420b54c64a00c0cac9729db4a328a648
Luca Andaloro <luca.andaloro4@gmail.com>
2022-04-26 15:30:33 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Laravel primi passi</title>
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <h1>
+            LARAVEL PRIMI PASSI
+        </h1>
+    </header>
+    <main>
+        <h2>Hello world!</h2>
+    </main>
+</body>
+</html>
